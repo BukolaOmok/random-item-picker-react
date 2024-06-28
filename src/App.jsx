3ad0e6373ec4b681865b54c969120196cd0373e6
2405-005-bo <omokehindeoluwabukola@gmail.com>
2024-06-28 15:27:59 +0100

    add static page

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,24 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-
   return (
-    <div>
+    <div className = "content-style">
       <div>
-      <h1>
-        Bukola Simple To-do List App
-      </h1>
+        <h1>Bukola Simple To-do List App</h1>
       </div>
 
       <div>
-      <input
-            type="text"
-            placeholder="Add a new task..."
-          />
-          <button onClick="">Add</button>
-    </div>
+        <input type="text" placeholder="Add a new task..." />
+        <button onClick="">Add</button>
+      </div>
 
-    <div>
-      <h2>Current Tasks</h2>
-    </div>
+      <div>
+        <h2>Current Tasks</h2>
+      </div>
 
-    <div>
-      <h2>Completed Tasks</h2>
+      <div>
+        <h2>Completed Tasks</h2>
+      </div>
     </div>
-        </div>
   );
 }
