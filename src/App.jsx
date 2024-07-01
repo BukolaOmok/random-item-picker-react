74332905306d6eb7310d72aa72cbc0d16bbb6223
2405-005-bo <omokehindeoluwabukola@gmail.com>
2024-07-01 15:04:11 +0100

    complete QR Code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 export default function App() {
   const [typedString, setTypedString] = React.useState("");
   const [qrCodeUrl, setQrCodeUrl] = React.useState("");
+  const [storeQRCode, setStoreQRCode] = React.useState([]);
 
   const handleChange = (event) => {
     setTypedString(event.target.value);
@@ -13,15 +14,28 @@ export default function App() {
   const generateQRCode = () => {
     QRCode.toDataURL(typedString, (err, url) => {
       setQrCodeUrl(url);
+      storeRecentFiveQRCode(url)
     });
   };
 
+  const storeRecentFiveQRCode = (url) => {
+      setStoreQRCode (storeQRCode => {
+        const newStoreQRCode = [url, ...storeQRCode];
+        return newStoreQRCode.slice(0,5)
+      })
+    }
+
+  
+
   return (
+    <div>
     <div className="content-style">
       <div>
         <h1>QR Code Generator</h1>
       </div>
+
       <p>Enter a URL and click generate to see the QR code</p>
+
       <div>
         <input
           type="text"
@@ -31,11 +45,20 @@ export default function App() {
         />
         <button onClick={generateQRCode}>Generate</button>
       </div>
-      <div>
-        {qrCodeUrl && (
-          <img src={qrCodeUrl} alt="QR Code" style={{ marginTop: "20px" }} />
-        )}
-      </div>
+
+      <div>{qrCodeUrl && <img src={qrCodeUrl} alt="QR Code" />}</div>
     </div>
-  );
-}
+
+<div className="stored-qr-style">
+<h1>Recent QR Codes Generated</h1>
+
+<div>
+{storeQRCode.map((qrCode, index) => (
+        <div key={index} className="qr-code">
+          <img src={qrCode} alt={`QR Code ${index + 1}`} />
+        </div>
+      ))}
+</div>
+</div>
+</div>
+)}
